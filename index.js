7206c11ddc9df6a22b8f1c508eea9610935de2b5
Md Firoj Kabir <firoj.kabir65@gmail.com>
2023-05-23 16:26:04 +0200

    Project initialize and add dependencies

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+
+const { router } = require("./routes");
+const { authRouter } = require('./routes/auth.route')
+const { errorHandler } = require('./middlewares/error.middleware');
+const { authMiddleware } = require('./middlewares/auth.middleware')
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DB_URL).then(
+	() => {
+		console.log('DB connected!')
+	},
+	(err) => {
+		console.log("Error", err)
+	}
+)
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan("common"))
+app.use("/auth", authRouter)
+app.use(authMiddleware)
+app.use("/", router)
+app.use(errorHandler)
+
+app.listen(port, () => {
+	console.log(`Social media app listening on port ${port}`)
+})
