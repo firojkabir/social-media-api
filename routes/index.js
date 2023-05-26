77e6e5eded6fa9ebda8cb09d96972307d28d2bb7
Md Firoj Kabir <firoj.kabir65@gmail.com>
2023-05-26 09:35:37 +0200

    Create model, controller and endpoints - comments

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const { userRouter } = require('./user.route')
 const { postRouter } = require('./post.route')
+const { commentRouter } = require('./comment.route')
 
 const router = express.Router()
 
 router.use('/users', userRouter)
 router.use('/posts', postRouter)
+router.use('/comments', commentRouter)
 
 module.exports = {
 	router
